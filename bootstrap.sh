6a31b0de6e54feb8177d51efd4fb565c24870cd9
chengleqi <chengleqi5g@hotmail.com>
2022-03-25 19:21:02 +0800

    添加配置

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,6 @@
 mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
-
 curl -o /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-7.repo
-
 yum makecache
-
+yum update
 yum install -y vim
-
 yum install -y net-tools
